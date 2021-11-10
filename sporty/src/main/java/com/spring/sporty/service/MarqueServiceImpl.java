afae8d0e927409520a201b162a4da6a36f39b15f
Manel03 <92320805+Manel03@users.noreply.github.com>
2021-11-10 13:23:13 +0100

    "Creation gestion des marques"

---
+++ sporty/src/main/java/com/spring/sporty/service/MarqueServiceImpl.java
@@ -0,0 +1,44 @@
+package com.spring.sporty.service;
+
+
+import com.spring.sporty.model.Categorie;
+import com.spring.sporty.model.Marque;
+import com.spring.sporty.repository.CategorieRepository;
+import com.spring.sporty.repository.MarqueRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+@Service
+public class MarqueServiceImpl implements MarqueService {
+
+    @Autowired
+    MarqueRepository agent;
+    @Override
+
+    public Marque add_marque(Marque m) {
+        return agent.save(m);
+    }
+
+    @Override
+    public Optional<Marque> find_marque(long id) {
+        return agent.findById(id);
+    }
+
+    @Override
+    public void delete_marque(long id) {
+        agent.deleteById(id);
+
+    }
+
+    @Override
+    public Marque update_marque(Marque m) {
+        return agent.save(m);
+    }
+
+    @Override
+    public List<Marque> findAll() {
+        return (List<Marque>) agent.findAll();
+    }
+}
